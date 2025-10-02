5fba0e36289a421d59e0dacef8c4b442708b3940
Saphereye <adarshdas950@gmail.com>
2025-10-02 12:47:05 +0530

    refactor: removed flatpak scripts

--- Scripts/install_pst.sh
+++ Scripts/install_pst.sh
@@ -64,22 +64,3 @@ fi
 
 # shell
 "${scrDir}/restore_shl.sh"
-
-# flatpak
-if ! pkg_installed flatpak; then
-    echo ""
-    print_log -g "[FLATPAK]" -b " list :: " "flatpak application"
-    awk -F '#' '$1 != "" {print "["++count"]", $1}' "${scrDir}/extra/custom_flat.lst"
-    prompt_timer 60 "Install these flatpaks? [Y/n]"
-    fpkopt=${PROMPT_INPUT,,}
-
-    if [ "${fpkopt}" = "y" ]; then
-        print_log -g "[FLATPAK]" -b " install :: " "flatpaks"
-        [ ${flg_DryRun} -eq 1 ] || "${scrDir}/extra/install_fpk.sh"
-    else
-        print_log -y "[FLATPAK]" -b " skip :: " "flatpak installation"
-    fi
-
-else
-    print_log -y "[FLATPAK]" -b " :: " "flatpak is already installed"
-fi
